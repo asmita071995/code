10fc21b2d318b3c1e8e940e1a533343beda80c1d
Asmita <asmitachauhan.ju@gmail.com>
2023-10-18 18:17:47 +0530

    addnewfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "pages#home"
- 
-  resources :article, only: [:show]  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   root 'pages#home'
+   get 'about', to: 'pages#about'
+   resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 end
+
