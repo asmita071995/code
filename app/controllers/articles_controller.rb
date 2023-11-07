3ed1878bf9976d1ede5907cbc6ffd4ea8781aedd
Asmita <asmitachauhan.ju@gmail.com>
2023-11-07 19:00:37 +0530

    newaddfile

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,46 +1,60 @@
 class ArticlesController < ApplicationController
-    
-    def show
-      @article = Article.find(params[:id])
-       
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
+  def show
+  end
+
+  def index
+    @articles = Article.paginate(page: params[:page], per_page: 5)
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
     end
+  end
 
-    def index
-      @article = Article.all
-    
-    end  
-
-    def new
-      @article = Article.new
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
     end
+  end
 
-    def edit
-      @article = Article.find(params[:id])
-    
-    
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description, category_ids: [])
+  end
+
+  def require_same_user
+    if current_user != @article.user && !current_user.admin?
+      flash[:alert] = "You can only edit or delete your own article"
+      redirect_to @article
     end
-
-    def create
-      @article = Article.new(params.require(:article).permit(:title, :description))
-      if  @article.save
-          flash[:notice] = "Article was created successfully."
-        redirect_to article_path(@article)
-      else
-         render 'new'
-      end
-    end
-
-    def update
-      @article = Article.find(params[:id])  # Assuming you're updating an existing article
-    
-      if @article.update(params.require(:article).permit(:title, :description))
-        flash[:notice] = "Article was updated successfully."
-        redirect_to @article
-      else
-        render 'edit'
-      end
-    end
-    
-
-
-end  
+  end
